26922c15e05e27d9ed8d7c6d7628f0bbdbec3d64
il <sh.il252255@gmail.com>
2023-01-28 15:11:56 +0800

    feat: add column_name_conv.

--- Parser/imdb/tsv_converters.py
+++ Parser/imdb/tsv_converters.py
@@ -1,6 +1,10 @@
 from numpy import nan
 
 
+def column_name_conv(name: str) -> str:
+    return ''.join([f'_{n.lower()}' if n.isupper() else n for n in name])
+
+
 def tconst_conv(tconst: str) -> int:
     return int(tconst[2:])
 
